13e293eee7d6a2205e57bf16176753b80a5ac582
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-17 23:44:57 -0600

    Enabale strict mode, adding state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+import TodoContainer from "./components/TodoContainer";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+  <TodoContainer />
+  </React.StrictMode>,  
+  document.getElementById("root")
+)
