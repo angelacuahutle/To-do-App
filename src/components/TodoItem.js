11e4a74b252de5cd3a0afaf6853d67f63d325d6e
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-18 12:11:29 -0600

    Delete auto-generated files

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,12 @@
 import React from "react"
 
 function TodoItem(props) {
-  return <li>{props.todo.title}</li>
+  return (
+    <li>
+      <input type="checkbox" checked= {this.props.todo.completed} onChange={() => console.log("clicked")}
+      />
+    </li>
+  )
 }
 
 export default TodoItem
