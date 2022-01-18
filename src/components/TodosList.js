934051f69680ccd76653823b9c19f6b8b83a19bb
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-18 12:54:20 -0600

    Add form and uuid to do so

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -2,15 +2,39 @@ import React from "react"
 import TodoItem from "./TodoItem";
 
 class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    )
-  }
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
+render() {
+  return (
+    <ul>
+      {this.state.todos.map((todo) => (
+         <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={this.props.handleChangeProps}
+          deleteTodoProps={this.props.deleteTodoProps}
+        />
+      ))}
+    </ul>
+  );
 }
 
-export default TodosList
+export default TodosList;
